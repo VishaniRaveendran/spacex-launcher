c7bca3d9feb5c27840745d7c6e09647bcdbcddfa
Vishani Raveendran <66343684+VishaniRaveendran@users.noreply.github.com>
2025-09-27 23:37:49 +0530

    added constant file

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const LAUNCHES_API = "https://api.spacexdata.com/v4/launches";
+export const ROCKET_API_BASE = "https://api.spacexdata.com/v4/rockets/";
+export const LAUNCHPAD_API_BASE = "https://api.spacexdata.com/v4/launchpads/";
+export const PAYLOAD_API_BASE = "https://api.spacexdata.com/v4/payloads/";
+export const BASE_URL = "https://api.spacexdata.com/v4/";
+
